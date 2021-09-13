3bf0a24c5541979a6c3352b1202ef545bd9816ac
Игорь Чехунов <igorcehunov@MacBook-Pro-Igor.local>
2021-09-14 02:05:08 +0300

    added img and fixed bugs

--- src/pages/Base.vue
+++ src/pages/Base.vue
@@ -10,8 +10,8 @@
       </p>
     </div>
     <div class="imgs">
-      <img src="../img/Subtract.png" class="subtract" />
-      <img src="img/Subtract_2.png" class="subtract2" />
+      <img :src="getImgUrl(substracFirst)" class="subtract" />
+      <img :src="getImgUrl(substracSecond)" class="subtract2" />
       <div class="Ellipse4"></div>
     </div>
   </div>
@@ -27,7 +27,7 @@
     <p class="keyMon">Кошелёк криптовалют</p>
     <div class="coins">
       <div class="coin" id="input0">
-        <img :src="img" v-for="(i, index) in gold" :key="index" alt="coin" />
+        <img :src="getImgUrl(imgCoin)" v-for="(i, index) in gold" :key="index" alt="coin" />
       </div>
     </div>
 
@@ -265,8 +265,113 @@ export default {
   name: 'Base',
   data () {
     return {
-    //   substracFirst: require('../img/Subtract.png')
+      substracFirst: 'Subtract',
+      substracSecond: 'Subtract_2',
+      imgCoin: 'coin',
+      checked: false,
+      isHasMoneyAndAllDetails: true,
+      gold: 99,
+      shop: [{
+        title: 'Биомеханизм',
+        price: 7,
+        sold: 5,
+        img: './img/Subtract3.png'
+      },
+      {
+        title: 'Процессор',
+        price: 5,
+        sold: 3,
+        img: './img/Vector2.png'
+      },
+      {
+        title: 'Душа',
+        price: 25,
+        sold: 15,
+        img: './img/Soul .png'
+      }
+      ],
+      item2: 4,
+      item3: 1,
+      items: [],
+      pickedType: 'FrontEnd',
+      pickedgender: 'Male'
+    }
+  },
+  methods: {
+    getImgUrl (image) {
+      return `/img/${image}.png`
+    },
+    addGold: function () {
+      if (this.gold >= 100) {
+        // openModal.style.display = 'block'
+        return
+      }
+      this.gold += this.checked ? 5 : 1
+    },
+    buy: function (index) {
+      const item = this.shop[index]
+      if (item.price > this.gold) {
+        // NotModal.style.display = 'block'
+        return
+      }
+      const i = this.items.findIndex(obj => obj.index === index)
+      if (i >= 0) {
+        this.items[i].count += 1
+      } else {
+        this.items.push({
+          count: 1,
+          index: index
+        })
+      }
+      this.gold -= item.price
+    },
+    sold: function (index) {
+      const item = this.items[index]
+      if (item.count <= 0) {
+        return
+      }
+      const shopItem = this.shop[item.index]
+      this.gold += shopItem.sold
+      item.count -= 1
+    }
+  },
+  mounted () {
+    for (let i = 0; i < this.shop.length; i++) {
+      this.items.push({ index: i, count: 0 })
+    }
+  },
+  computed: {
+    Robot () {
+      if ((this.pickedType === 'FrontEnd') && (this.pickedgender === 'Female')) {
+        return './img/nofrontFemale.png'
+      } else if ((this.pickedType === 'FrontEnd') && (this.pickedgender === 'Male')) {
+        return './img/nofrontMale.png'
+      } else if ((this.pickedType === 'Design') && (this.pickedgender === 'Female')) {
+        return './img/noDesignerFemale.png'
+      } else {
+        return './img/noDesignerMale.png'
+      }
+    },
+    item1 () {
+      return 1
     }
   }
 }
+// document.addEventListener('DOMContentLoaded', function () {
+//   var scrollbar = document.body.clientWidth - window.innerWidth + 'px'
+//   document.querySelector('[href="#openModal"]').addEventListener('click', function () {
+//     document.body.style.overflow = 'hidden'
+//     document.querySelector('#openModal').style.marginLeft = scrollbar
+//   })
+//   document.querySelector('[href="#close"]').addEventListener('click', function () {
+//     document.body.style.overflow = 'visible'
+//     document.querySelector('#openModal').style.marginLeft = '0px'
+//   })
+// })
+// document.getElementById('close').onclick = function () {
+//   document.getElementById('openModal').style.display = 'none'
+// }
+// document.getElementById('closer').onclick = function () {
+//   document.getElementById('NotModal').style.display = 'none'
+// }
 </script>
